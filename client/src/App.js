4a627742cebe9e36d6cabeee4a6a4e36be57659e
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-09-14 16:31:47 -0500

    finished adding Routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Route, Link, Switch} from 'react-router-dom'
+
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -29,6 +33,14 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
+      <Switch>
+      <Route path = '/'>
+        <MovieList/>
+      </Route>
+      <Route path='/movies/:id'>
+        <Movie/>
+      </Route>
+      </Switch>
 
       <div>Replace this Div with your Routes</div>
     </div>
